c78f5860050195012d2b7634135ed0c5fb6c3a1c
dmolloc1 <139632142+dmolloc1@users.noreply.github.com>
2025-12-16 13:11:10 -0500

    Default receta branch from current user

--- backend/recetas/views.py
+++ backend/recetas/views.py
@@ -36,5 +36,8 @@ class RecetaViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, IsOptometra]
 
     def perform_create(self, serializer):
-        serializer.save(usuCod=self.request.user)
+        # Fill in branch automatically when it is omitted so we avoid DB errors
+        # when the client forgets to send it.
+        sucur = serializer.validated_data.get('sucurCod') or getattr(self.request.user, 'sucurCod', None)
+        serializer.save(usuCod=self.request.user, sucurCod=sucur)
 
